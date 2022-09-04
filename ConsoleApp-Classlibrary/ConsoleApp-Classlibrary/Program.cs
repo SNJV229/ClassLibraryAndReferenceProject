6c8dc72b9de7de4209a3cf954bbc50899f30a972
SNJV229 <38901682+SNJV229@users.noreply.github.com>
2022-09-05 00:16:34 +0530

    Created classLibrary and refer project

---
+++ ConsoleApp-Classlibrary/ConsoleApp-Classlibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classlibrary;
+using System.Windows.Forms;
+
+namespace ConsoleApp_Classlibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo ob = new Demo();
+            ob.message();
+        }
+    }
+}
